97b2c83c0ffd8e93200ac997605fd9129ad732ad
ivankodrnja <ikodrnja@gmail.com>
2016-07-27 07:39:36 +0200

    feat: map tabbed detail call out transition to house detail view

--- Apartments Croatia/MapTabViewController.swift
+++ Apartments Croatia/MapTabViewController.swift
@@ -8,34 +8,143 @@
 
 import UIKit
 import MapKit
-import CoreLocation
+import CoreData
+
 
 class MapTabViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
-
-    // latitude and longitude will be set from the previous VC
-    var latitude: Double?
-    var longitude: Double?
+    
+    
+    // init of the FBClusterManager
+    let clusteringManager = FBClusteringManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        latitude = 45.34054
-        longitude = 15.4757
-        
-        // Do any additional setup after loading the view.
-        
+        let array:[MKAnnotation] = houseLocations()
+        clusteringManager.addAnnotations(array)
+        clusteringManager.delegate = self
+        // show the map
+        let latitude = 44.281863
+        let longitude = 16.382595
         mapView.mapType = MKMapType.Hybrid
-        
-        // draw the location of the house
-        let location = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
-        let span = MKCoordinateSpanMake(6.9, 6.9)
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpanMake(7.9, 7.9)
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
+        /*
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
-        mapView.addAnnotation(annotation)
+         */
+    }
+    
+    // MARK: - Core Data Convenience
+    
+    var sharedContext: NSManagedObjectContext {
+        return CoreDataStackManager.sharedInstance().managedObjectContext
+    }
+    
+    func getAllLatLng() -> [House] {
+        
+        let getAllLatLngFetchRequest = NSFetchRequest(entityName: "House")
+        let allLatLng = (try! sharedContext.executeFetchRequest(getAllLatLngFetchRequest)) as! [House]
+        
+        return allLatLng
+        
+    }
+    
+    // MARK: - Utility
+    
+    func houseLocations() -> [FBAnnotation] {
+        var array:[FBAnnotation] = []
+        let allLatLng = getAllLatLng()
+        
+        for house in allLatLng {
+            let a:FBAnnotation = FBAnnotation()
+            a.coordinate = CLLocationCoordinate2D(latitude:house.latitude, longitude: house.longitude )
+            a.title = house.name
+            a.house = house
+            array.append(a)
+        }
+
+        return array
+    }
+    
+}
+
+    
+extension MapTabViewController : FBClusteringManagerDelegate {
+    
+    func cellSizeFactorForCoordinator(coordinator:FBClusteringManager) -> CGFloat{
+        return 1.0
+    }
+    
+}
+
+
+extension MapTabViewController : MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool){
+        
+        NSOperationQueue().addOperationWithBlock({
+            
+            let mapBoundsWidth = Double(self.mapView.bounds.size.width)
+            
+            let mapRectWidth:Double = self.mapView.visibleMapRect.size.width
+            
+            let scale:Double = mapBoundsWidth / mapRectWidth
+            
+            let annotationArray = self.clusteringManager.clusteredAnnotationsWithinMapRect(self.mapView.visibleMapRect, withZoomScale:scale)
+            
+            self.clusteringManager.displayAnnotations(annotationArray, onMapView:self.mapView)
+        
+            
+        })
+        
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var reuseId = ""
+        
+        if annotation.isKindOfClass(FBAnnotationCluster) {
+            
+            reuseId = "Cluster"
+            var clusterView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+            clusterView = FBAnnotationClusterView(annotation: annotation, reuseIdentifier: reuseId, options: nil)
+            
+            return clusterView
+            
+        } else {
+            
+            reuseId = "Pin"
+            var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+            
+            pinView!.pinTintColor = UIColor.greenColor()
+            
+            return pinView
+        }
+        
+    }
+    
+    func mapView(mapView: MKMapView, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == annotationView.rightCalloutAccessoryView {
+            let controller = storyboard!.instantiateViewControllerWithIdentifier("HouseDetailTableViewController") as! HouseDetailTableViewController
+            let object = annotationView.annotation as! FBAnnotation
+            let house = object.house
+            print(house)
+            // set destination object in the detail VC
+            controller.house = house!
+            
+            self.navigationController!.pushViewController(controller, animated: true)
+
+        }
     }
     
 }
