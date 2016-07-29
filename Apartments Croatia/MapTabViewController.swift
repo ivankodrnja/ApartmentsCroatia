fc1c24fb3c5ab949e2c7e5677926f899ed8d3e45
ivankodrnja <ikodrnja@gmail.com>
2016-07-29 08:04:37 +0200

    feat: nearby functionality completed

--- Apartments Croatia/MapTabViewController.swift
+++ Apartments Croatia/MapTabViewController.swift
@@ -131,31 +131,97 @@ class MapTabViewController: UIViewController {
         
     }
     
+    func showNearbyMapRect(latitude latitude: Double, longitude: Double) {
+        
+        mapView.showsPointsOfInterest = false
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpanMake(1.1, 1.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+
 
     
     @IBAction func segmentedControlAction(sender: AnyObject) {
         
-        
-        
+
         switch sender.selectedSegmentIndex {
+        // show all houses
         case 0:
+            // delete all previous annotations so they don't duplicate, depending if previous selection was wishlist or nearby
+            if (self.segmentedControlIndex == 1){
+                if wishlistHousesAnnotationsArray != nil {
+                    mapView.removeAnnotations(wishlistHousesAnnotationsArray!)
+                    clusteringManager.removeAnnotations(wishlistHousesAnnotationsArray!)
+                }
+            }
+            if (self.segmentedControlIndex == 2){
+                if allHousesAnnotationsArray != nil {
+                    mapView.removeAnnotations(allHousesAnnotationsArray!)
+                    clusteringManager.removeAnnotations(allHousesAnnotationsArray!)
+                }
+            }
             self.segmentedControlIndex = 0
-            mapView.removeAnnotations(wishlistHousesAnnotationsArray!)
-            clusteringManager.removeAnnotations(wishlistHousesAnnotationsArray!)
+
+            
             clusteringManager.addAnnotations(allHousesAnnotationsArray!)
             self.showMapRect(latitude: 44.281863, longitude: 16.382595)
-            
+        // show houses from the wishlist
         case 1:
+            // delete all previous annotations so they don't duplicate
+            if (self.segmentedControlIndex == 0 || self.segmentedControlIndex == 2){
+                if allHousesAnnotationsArray != nil {
+                    mapView.removeAnnotations(allHousesAnnotationsArray!)
+                    clusteringManager.removeAnnotations(allHousesAnnotationsArray!)
+                }
+            }
             self.segmentedControlIndex = 1
-            self.showHousesFromWishlist()
-            mapView.removeAnnotations(allHousesAnnotationsArray!)
-            clusteringManager.removeAnnotations(allHousesAnnotationsArray!)
+            
+            if wishlistHousesAnnotationsArray != nil {
+                self.showHousesFromWishlist()
+            }
             self.showMapRect(latitude: 44.281862, longitude: 16.382594)
-            
+        
+        // show nearby houses
         case 2:
+            // delete all previous annotations so they don't duplicate
+            if (self.segmentedControlIndex == 0){
+                if allHousesAnnotationsArray != nil {
+                    mapView.removeAnnotations(allHousesAnnotationsArray!)
+                    clusteringManager.removeAnnotations(allHousesAnnotationsArray!)
+                }
+            }
+            if (self.segmentedControlIndex == 1){
+                if wishlistHousesAnnotationsArray != nil {
+                    mapView.removeAnnotations(wishlistHousesAnnotationsArray!)
+                    clusteringManager.removeAnnotations(wishlistHousesAnnotationsArray!)
+                }
+            }
+            clusteringManager.addAnnotations(allHousesAnnotationsArray!)
             self.segmentedControlIndex = 2
-            return
+            // check for location services
+            if CLLocationManager.locationServicesEnabled() {
+                switch(CLLocationManager.authorizationStatus()) {
+                case .NotDetermined:
+                    locationManager.delegate = self
+                    locationManager.requestWhenInUseAuthorization()
+                    
+                case .AuthorizedAlways, .AuthorizedWhenInUse:
+                    locationManager.delegate = self
+                    locationManager.startUpdatingLocation()
+                    self.showNearbyMapRect(latitude: NetworkClient.sharedInstance().userLocationLatitude, longitude: NetworkClient.sharedInstance().userLocationLongitude)
+                
+                case .Denied:
+                    self.openSettingsToEnableLocationService()
+                    
+                case .Restricted:
+                    self.openSettingsToEnableLocationService()
+                }
+            }
             
+            
+        // choose map style
         case 3:
             // selected segment will remain the one which was previously selected
             self.segmentedControl.selectedSegmentIndex = self.segmentedControlIndex
@@ -168,20 +234,17 @@ class MapTabViewController: UIViewController {
     func chooseMapStyle() {
         
         let alert = UIAlertController(title: "Choose Map Style", message: nil, preferredStyle: .Alert) // 1
+        
         let firstAction = UIAlertAction(title: "Standard", style: .Default) { (alert: UIAlertAction!) -> Void in
             self.mapView.mapType = MKMapType.Standard
         }
-        
         let secondAction = UIAlertAction(title: "Satellite", style: .Default) { (alert: UIAlertAction!) -> Void in
             self.mapView.mapType = MKMapType.Satellite
         }
-        
         let thirdAction = UIAlertAction(title: "Hybrid", style: .Default) { (alert: UIAlertAction!) -> Void in
             self.mapView.mapType = MKMapType.Hybrid
         }
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (alert: UIAlertAction!) -> Void in
-            
         }
         
         alert.addAction(firstAction)
@@ -192,9 +255,26 @@ class MapTabViewController: UIViewController {
         presentViewController(alert, animated: true, completion:nil)
         
     }
+    
+    func openSettingsToEnableLocationService(){
+        let alertController = UIAlertController (title: "Please enable location services in Settings", message: "Go to Settings?", preferredStyle: .Alert)
+        
+        let settingsAction = UIAlertAction(title: "Settings", style: .Default) { (_) -> Void in
+            let settingsUrl = NSURL(string: UIApplicationOpenSettingsURLString)
+            if let url = settingsUrl {
+                UIApplication.sharedApplication().openURL(url)
+            }
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
+    alertController.addAction(settingsAction)
+    alertController.addAction(cancelAction)
+    
+    presentViewController(alertController, animated: true, completion: nil)
+    }
 }
 
-    
+
 extension MapTabViewController : FBClusteringManagerDelegate {
     
     func cellSizeFactorForCoordinator(coordinator:FBClusteringManager) -> CGFloat{
@@ -203,6 +283,7 @@ extension MapTabViewController : FBClusteringManagerDelegate {
     
 }
 
+// MARK: - Map View Delegate
 
 extension MapTabViewController : MKMapViewDelegate {
     
@@ -240,6 +321,7 @@ extension MapTabViewController : MKMapViewDelegate {
             // show user location as a blue dot
         } else if annotation.isKindOfClass(MKUserLocation) {
             return nil
+            // show houses as green pins
         } else  {
             reuseId = "Pin"
             var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
@@ -271,6 +353,8 @@ extension MapTabViewController : MKMapViewDelegate {
     
 }
 
+// MARK: - Location Delegate
+
 extension MapTabViewController: CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -281,5 +365,12 @@ extension MapTabViewController: CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print(error)
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse || status == .AuthorizedAlways {
+            manager.startUpdatingLocation()
+
+        }
     }
 }
